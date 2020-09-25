3b7f1e30f95bdb22bb1f9b0680cf33b3e30bbb9e
Mrinal Singh <mrinal.singh@cerner.com>
2020-09-25 22:52:19 +0530

    Floor sqrt

---
+++ Floor.php
@@ -0,0 +1,26 @@
+<?php 
+// A PHP program to find floor(sqrt(x) 
+  
+// Returns floor of square root of x 
+function floorSqrt($x) 
+{ 
+    // Base cases 
+    if ($x == 0 || $x == 1) 
+    return $x; 
+  
+    // Staring from 1, try all  
+    // numbers until i*i is  
+    // greater than or equal to x. 
+    $i = 1; 
+    $result = 1; 
+    while ($result <= $x) 
+    { 
+        $i++; 
+        $result = $i * $i; 
+    } 
+    return $i - 1; 
+} 
+  
+// Driver Code 
+$x = 11; 
+echo floorSqrt($x), "\n";
